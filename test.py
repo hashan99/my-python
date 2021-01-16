c03ea8878e51c7867fdb185ac70ca18422dfe2c4
Hashan Kumarasinghe <54454767+hashan99@users.noreply.github.com>
2021-01-16 20:27:36 +0530

    test python code

---
+++ test.py
@@ -0,0 +1,55 @@
+# x = input("Enter a number -> ")
+# print("The number entered", x)
+
+# a,b = 2, "abc"
+# print("a = " + str(a) + " b = " + str(b))
+
+# a = 1,2,3,"abc"
+# print("a = " + str(a) + " b = " + str(b))
+
+# x = input("Enter the first number -> ") 
+# y = input("Enter the second number -> ") 
+# z = x+y
+# print(z)
+
+# x = int(input("Enter the first number -> ")) 
+# y = int(input(("Enter the second number -> "))
+# z = p+q
+# print(r)
+
+#print(int(input("Enter number 1 -> ")) + int(input("Enter number 2 ->")))
+
+# x = input("Enter a number -> ")
+# a = int(x)
+
+# if(a>100) :
+# 	print("a in greater than 100")
+# else :
+# 	print("a is less than 100")
+
+# a = input("Enter the first number -> ")
+# b = input("Enter the second number -> ")
+# action = input("Enter the action letter (a / c) ->")
+# if(action == "a") :
+# 	print("The sum of numbers = ", int(a)+int(b))
+# elif(action == "c") :
+# 	print("The concatenation of numbers", a+b)
+# else :
+# 	print("Invalid input")
+
+# for  c in "Hashan" :
+# 	print(c)
+
+# s = "Imagine"
+# t = "Dragons"
+
+# print("a in s = ", 'a' in s)
+# print("z in s = ", 'z' in s)
+# print("s+t = ", s+t)
+# print("s*3 = ", s*3)
+
+a = [1, 1.2, "abc", [1,2,"abc"]]
+print(a)
+b = [10,20,30]
+print(b)
+print(a+b)
